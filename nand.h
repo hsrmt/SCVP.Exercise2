7ac580dfafd77e2a663ab8b23cc72c0b30a1a40d
hatice seremet <haticeesrmt52@gmail.com>
2024-03-18 08:57:50 -0400

    nand.h

--- nand.h
+++ nand.h
@@ -5,5 +5,21 @@
 
 
 // TODO: Insert your code here
+SC_MODULE(nand){
+    sc_in<bool> A, B;
+    sc_out<bool> Z;
+
+    void do_nand(){
+        Z.write(!(A.read() & B.read()));
+    }
+
+
+    SC_CTOR(nand){
+        SC_METHOD(do_nand);
+        sensitive << A << B;
+    }
+
+
+};
 
 #endif
